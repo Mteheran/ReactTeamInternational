0367368bf76770aee03e8ebd0c39f4f80342e28f
Miguel Teheran <miguelteheran@gmail.com>
2022-09-25 21:51:29 -0500

    adding base react app for team international

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './Logo_Team_International.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Workshop team international
+        </p>
+        <p>
+          Find <a href='https://www.linkedin.com/company/team-international/jobs/'>Jobs</a>.
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default App;
